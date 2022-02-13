901263ac40a4707672e381905319e2734e416fb9
MoMutawe1 <mutalogix@gmail.com>
2022-02-12 21:30:54 -0600

    publishing messages using routingKey

--- notification/src/main/java/com/myservices/clients/notification/notification/NotificationApplication.java
+++ notification/src/main/java/com/myservices/clients/notification/notification/NotificationApplication.java
@@ -1,11 +1,28 @@
 package com.myservices.clients.notification.notification;
 
+import com.myservices.amqp.RabbitMQMessageProducer;
+import com.myservices.clients.notification.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.myservices.clients.notification",
+                "com.myservices.amqp"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    //inject the RabbitMQMessageProducer
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig){
+      return args -> {
+            producer.publish("foo",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey());
+       };
     }
 }
